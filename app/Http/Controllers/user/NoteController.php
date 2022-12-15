4c966e500a4b51fd14acae7df0dc589f3c205165
haider10954 <chhaider10954@gmail.com>
2022-12-15 19:31:07 +0500

    API Response Store against Title

--- app/Http/Controllers/user/NoteController.php
+++ app/Http/Controllers/user/NoteController.php
@@ -13,9 +13,6 @@ class NoteController extends Controller
     {
 
         $notes = Note::where('user_id', auth()->id())->get();
-        // $response = Http::asForm()->post('http://3.140.248.219:8000/predict_api', [
-        //     'text' => 'Hello',
-        // ]);
         return view('user.notes', compact('notes'));
     }
     function upload_files($file)
@@ -36,9 +33,16 @@ class NoteController extends Controller
 
         $file = $this->upload_files($request['file']);
 
+        $response = Http::asForm()->post('http://3.140.248.219:8000/predict_api', [
+            'text' => $request['title'],
+        ]);
+        $getData = $response->collect()->toArray();
+
         $note = Note::create([
             'user_id' => auth()->id(),
             'title' => $request['title'],
+            'response' => json_encode($getData),
+            'analytics' => json_encode($getData['predictions']),
             'description' => $request['description'],
             'image' => $file,
         ]);
