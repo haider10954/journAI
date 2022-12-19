cefcceafd6937b4c5a99d4646734f7f312aaaf30
haider10954 <chhaider10954@gmail.com>
2022-12-19 15:18:36 +0500

    Modal Centered

--- resources/views/user/notes.blade.php
+++ resources/views/user/notes.blade.php
@@ -119,7 +119,7 @@
 
 
 <div class="modal" id="aiModal" tabindex="-1" aria-labelledby="aiModal" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">AI Analytics</h5>
