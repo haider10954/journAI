0f19e82282565895a203ecb0f7da05cebfeb7ae9
haider10954 <chhaider10954@gmail.com>
2022-12-19 19:18:50 +0500

    Error Fixed

--- resources/views/user/notes.blade.php
+++ resources/views/user/notes.blade.php
@@ -172,6 +172,24 @@
     </div>
 </div>
 
+<div class="modal fade zoomIn" id="suggestionModal" tabindex="-1" aria-labelledby="suggestionModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">AI Suggestion</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p id="response-text"></p>
+            </div>
+            <div class="modal-footer">
+                <div class="d-flex align-items-end justify-content-end">
+                    <button class="btn btn-sm btn-info response_suggestion" data-bs-toggle="modal" data-suggestion="{{ $response->suggestions }}" href="#suggestionModal">Suggestion</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <div class="modal fade" id="createboardModal" tabindex="-1" aria-labelledby="createboardModalLabel" aria-hidden="true">
@@ -224,31 +242,6 @@
     </div>
 </div>
 <!--end add note modal-->
-
-
-<div class="modal fade zoomIn" id="deleteRecordModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="delete-btn-close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="mt-2 text-center">
-                    <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json" trigger="loop" colors="primary:#f7b84b,secondary:#f06548" style="width:100px;height:100px"></lord-icon>
-                    <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
-                        <h4>Are you sure ?</h4>
-                        <p class="text-muted mx-4 mb-0">Are you sure you want to remove this tasks ?</p>
-                    </div>
-                </div>
-                <div class="d-flex gap-2 justify-content-center mt-4 mb-2">
-                    <button type="button" class="btn w-sm btn-light" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn w-sm btn-danger" id="delete-record">Yes, Delete It!</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!--end modal -->
 
 <!-- Edit Note -->
 <div class="modal fade zoomIn" id="editModalNote" tabindex="-1" aria-hidden="true">
