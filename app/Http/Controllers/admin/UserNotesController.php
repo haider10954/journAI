572a3f49c18f3b1aa35118e9800199fddae45951
haider10954 <chhaider10954@gmail.com>
2022-12-23 12:24:10 +0500

    Condition Changed

--- app/Http/Controllers/admin/UserNotesController.php
+++ app/Http/Controllers/admin/UserNotesController.php
@@ -10,7 +10,7 @@ class UserNotesController extends Controller
 {
     public function index()
     {
-        $notes = Note::paginate(1);
+        $notes = Note::latest()->paginate(6);
         return view('admin.note', compact('notes'));
     }
 
