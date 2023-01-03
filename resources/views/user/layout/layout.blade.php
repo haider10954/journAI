7c9a638210dba4ab7880586c306a2d2a9b9eca5f
haider10954 <chhaider10954@gmail.com>
2023-01-03 13:03:58 +0500

    Expert Option removed

--- resources/views/user/layout/layout.blade.php
+++ resources/views/user/layout/layout.blade.php
@@ -141,11 +141,11 @@
     <!--end back-to-top-->
 
     <!-- Open Experts -->
-    <div class="customizer-setting d-none d-md-block">
+    <!-- <div class="customizer-setting d-none d-md-block">
         <div class="btn-info btn-rounded shadow-lg btn btn-icon btn-lg p-2" data-bs-toggle="offcanvas" data-bs-target="#theme-settings-offcanvas" aria-controls="theme-settings-offcanvas">
             <i class="ri-map-pin-user-line fs-22"></i>
         </div>
-    </div>
+    </div> -->
     <!--End Open Experts -->
 
     @include('user.includes.expert_menu')
